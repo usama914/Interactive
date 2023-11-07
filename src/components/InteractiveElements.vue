1ba05a0261ff9069c9a249fd4ac3cdfd408751c9
usama914 <usamanosherwan729@gmail.com>
2023-11-07 23:41:01 +0500

    finished

--- src/components/InteractiveElements.vue
+++ src/components/InteractiveElements.vue
@@ -28,7 +28,7 @@
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/dashboard">
+              <router-link to="/">
                 <button class="button is-primary">Dashboard</button>
               </router-link>
             </div>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import { createRouter , createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import InteractiveElements from './components/InteractiveElements.vue';
-import VisualInteractionSelector from './components/VisualSelector.vue';
-const router =createRouter({
+// import VisualInteractionSelector from './components/VisualSelector.vue';
+const router = createRouter({
     history: createWebHistory(),
-    routes:[
+    routes: [
         { path: '/', component: InteractiveElements },
-        { path: '/dashboard', component: VisualInteractionSelector }
+        // { path: '/dashboard', component: VisualInteractionSelector }
     ]
 })
 export default router
